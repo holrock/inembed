17cd9ecd3b6e003a1b56916cde2e2257c0e805fd
holrock <ispeporez@gmail.com>
2016-08-02 15:27:00 +0900

    remove /

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
   var base = tabs[0].url;
   var r = new RegExp('^https://www.instagram.com/p/[^/]+/');
   base = base.match(r);
-  var url = '<iframe src="*/embed" frameborder="0" width="70%" height="600px"></iframe>';
+  var url = '<iframe src="*embed" frameborder="0" width="70%" height="600px"></iframe>';
   edit.value = url.replace('*', base);
 });
 
